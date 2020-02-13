50202a3893b561b0af66c0028e57cea2ece626b2
therexone <renerjake@gmail.com>
2020-02-13 12:32:20 +0530

    exp1-aoa refactor

--- AOA/exp1-selectionSort.c
+++ AOA/exp1-selectionSort.c
@@ -8,35 +8,38 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
-void main() 
+void selectionSort(int array[], int n)
 {
-    int array[size];
-    printf("Enter elements of the array\n");
-    for( int i = 0; i < size; i++)
-    {
-        printf("array[%d]: ", i);
-        scanf("%d", &array[i]);
-    }
-    int i = 0;
-
-    for(i = 0; i < size-1; i++)
+    for (int i = 0; i < size - 1; i++)
     {
         int min_index = i;
-        for(int j = i+1; j < size; j++)
+        for (int j = i + 1; j < size; j++)
         {
-            if(array[j] < array[min_index])
+            if (array[j] < array[min_index])
                 min_index = j;
         }
         swap(&array[min_index], &array[i]);
     }
+}
 
+void main()
+{
+    int array[size];
+    printf("Enter elements of the array\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("array[%d]: ", i);
+        scanf("%d", &array[i]);
+    }
+    
+    selectionSort(array, size);
+    
     printf("Sorted array: \n");
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
         printf("%d\t", array[i]);
     }
     printf("\n");
-
 }
 
 // ---------OUTPUT---------
@@ -47,6 +50,6 @@ void main()
 // array[2]: 5
 // array[3]: 91
 // array[4]: 5
-// Sorted array: 
+// Sorted array:
 // 3       5       5       9       91
 // student@student-X556UQK:~/Desktop/workspace/sem4-uni/AOA$
